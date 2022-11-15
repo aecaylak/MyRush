cc0245fd0a553b1c1c1919f8ff0d82b7976562ff
Ali Emre <aliemrecaylak@gmail.com>
2022-11-15 20:32:17 +0300

    Initial commit

---
+++ Assets/Script/mobileTouchController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class mobileTouchController : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
+{
+    [SerializeField] private Image pivotImage;
+    private Vector2 _touchPosition;
+    public Vector2 _direction { get; private set; } //value can will be readed but cannot be changed
+    
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        pivotImage.transform.position = eventData.position;
+        pivotImage.gameObject.SetActive(true);
+        _touchPosition = eventData.position;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        _direction = Vector2.zero;
+        pivotImage.gameObject.SetActive(false);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        var delta = eventData.position - _touchPosition;
+        _direction = delta.normalized; //value's change from screen pixels to -1,1
+        Debug.Log(_direction);
+    }
+}
---
+++ Assets/Script/playerController.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    [SerializeField] private mobileTouchController input;
+    [SerializeField] private Rigidbody _rigidbody;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private Vector3 myDirection;
+    private void Move(Vector3 direction)
+    {
+        
+        _rigidbody.velocity = myDirection * moveSpeed * Time.deltaTime;
+    }
+
+    private void FixedUpdate()
+    {
+        myDirection = new Vector3(input._direction.x, 0, input._direction.y);
+        Move(myDirection);
+    }
+}
